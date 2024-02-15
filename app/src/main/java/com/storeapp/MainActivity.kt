ef773bd8163c0529b0e6f294d1c25313344bc680
Ophirmdhn <ophiramadhan18@gmail.com>
2024-02-15 10:03:42 +0800

    initial commit

---
+++ app/src/main/java/com/storeapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.storeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.storeapp.ui.theme.StoreAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StoreAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
